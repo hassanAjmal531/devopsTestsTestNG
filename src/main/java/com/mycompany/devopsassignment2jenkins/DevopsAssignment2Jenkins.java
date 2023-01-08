1366507a2bc40b89e51b30282ea33853179e4450
hassan ajmal <hassanajmal531@gmail.com>
2023-01-08 21:13:58 +0500

    first commit

---
+++ src/main/java/com/mycompany/devopsassignment2jenkins/DevopsAssignment2Jenkins.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.devopsassignment2jenkins;
+
+/**
+ *
+ * @author HP
+ */
+public class DevopsAssignment2Jenkins {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/TestNgTest.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+
+import org.openqa.selenium.By;
+import static org.testng.Assert.*;
+import org.testng.annotations.*;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+/**
+ *
+ * @author HP
+ */
+public class TestNgTest {
+    WebDriver driver = new ChromeDriver();
+    
+        
+    public TestNgTest() {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+        driver.get("https://hassanajmal531.github.io/");
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+       driver.close();
+        
+    }
+    
+    @Test
+    public void checkIfSubmitClickable() throws Exception {
+       WebElement button = driver.findElement(By.xpath(".//button[@id = 'contactBtn']"));
+        assertTrue(button.isDisplayed());
+        assertTrue(button.isEnabled());
+    }
+    
+    @Test
+    public void checkRedirectWithoutInfo()throws Exception {
+        
+      
+        WebElement button = driver.findElement(By.xpath(".//button[@id = 'contactBtn']"));
+        button.click();
+        WebElement error = driver.findElement(By.xpath("//*[@id=\"my-form\"]/div[1]/div[2]"));
+        assertTrue(error.isDisplayed());
+        
+        
+    }
+    
+    @Test
+    public void checkifRedirectwithInfo() throws Exception{
+       driver.findElement(By.xpath(".//input[@name = 'name']")).sendKeys("hassan");
+       driver.findElement(By.xpath(".//input[@name = 'email']")).sendKeys("hassan@gmail.com");
+       driver.findElement(By.xpath(".//textarea[@name = 'message']")).sendKeys("hello");
+       driver.findElement(By.xpath(".//button[@id = 'contactBtn']")).click();
+
+       Thread.sleep(5000);
+       String title = driver.getTitle();
+       assertEquals(title,"Formspree");
+
+    }
+    
+    @Test
+    public void checkEmail()throws Exception{
+        driver.findElement(By.xpath(".//input[@name = 'name']")).sendKeys("hassan");
+       driver.findElement(By.xpath(".//input[@name = 'email']")).sendKeys("abcsfedf");
+       driver.findElement(By.xpath(".//textarea[@name = 'message']")).sendKeys("hello");
+       driver.findElement(By.xpath(".//button[@id = 'contactBtn']")).click();
+       Thread.sleep(2000);
+       WebElement error = driver.findElement(By.xpath("//*[@id=\"my-form\"]/div[2]/div[2]"));
+       assertTrue(error.isDisplayed());
+    }
+    
+    @Test
+    public void checkResourcesButtonWorks() throws Exception{
+        WebElement ResourcesBtn = driver.findElement(By.xpath("//*[@id=\"navResourceBtn\"]"));
+        assertTrue(ResourcesBtn.isEnabled());
+        ResourcesBtn.click();
+        Thread.sleep(1000);
+        WebElement tag = driver.findElement(By.xpath("//*[@id=\"contriHead\"]"));
+        assertTrue(tag.isDisplayed());
+        
+    }
+}
